dc730dd9e4bb60ca9399f68f646f4c285d3d244c
dvorel <dvorel@gmail.com>
2019-03-13 18:47:33 +0100

    Create header.h

---
+++ header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+#define _CRT_SECURE_NO_WARNINGS
+
+float **alokacija(int, int);
+void popunjavanje(float**, int, int);
+void novaMatrica(float**, int, int, int);
+void ispis(float**, int, int);
+void brisanje(float**, int);
+
+
+
+#endif
